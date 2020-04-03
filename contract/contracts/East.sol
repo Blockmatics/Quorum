0e98bf242191d474caaab3cec220730d326ca619
gabrielibarra <gabej.ibarra@gmail.com>
2020-04-02 23:45:18 -0700

    gi/tutorial3 basic private transactions demo

--- contract/contracts/East.sol
+++ contract/contracts/East.sol
@@ -3,7 +3,7 @@ pragma solidity ^0.5.0;
 import "../node_modules/@openzeppelin/contracts/token/ERC20/ERC20.sol";
 import "../node_modules/@openzeppelin/contracts/token/ERC20/ERC20Detailed.sol";
 
-contract Public is ERC20, ERC20Detailed {
+contract East is ERC20, ERC20Detailed {
   uint public INITIAL_SUPPLY = 1000000;
 
   constructor(string memory _name, string memory _symbol)
